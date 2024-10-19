bec945dbc2b726a06f7b10995e8a92580feaa957
Sahil Dahal <denenme49@gmail.com>
2024-10-19 08:44:05 +0545

    feat: Add Modal component for displaying modals
    - Added a new file, Modal.jsx, under the Client/src/components/protected-home/sub-components directory.
    - The Modal component is a simple functional component that renders a div with the class "modal".
    
    Refactor Server index.js to use urlencoded with extended set to false
    
    - Modified the Server/index.js file.
    - Changed the express.urlencoded middleware to use the option "extended" set to false.

--- Server/index.js
+++ Server/index.js
@@ -15,7 +15,7 @@ const PostRoutes = require('./routes/postRoutes');
 
 // global middlewares
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(cors(
     {
         origin: process.env.CLIENT_URL,
